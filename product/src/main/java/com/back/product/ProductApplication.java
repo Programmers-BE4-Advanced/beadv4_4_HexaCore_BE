3686a3b44f619c5210f1f97d4a7060f9a0ba94f3
NamSangwon <nsw3265@naver.com>
2026-01-16 16:05:31 +0900

    refactor: 모듈 불러오는 형식 변경 - #28

--- product/src/main/java/com/back/product/ProductApplication.java
+++ product/src/main/java/com/back/product/ProductApplication.java
@@ -1,20 +1,15 @@
 package com.back.product;
 
-import com.back.security.config.BaseSecurityConfig;
-import com.back.security.jwt.JWTFilter;
-import com.back.security.jwt.JWTUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
-@EnableJpaAuditing
-@Import({ // Importing security configurations and utilities for use in the product module
-    BaseSecurityConfig.class,
-    JWTFilter.class,
-    JWTUtil.class
+@SpringBootApplication(scanBasePackages = {
+        "com.back.product",
+        "com.back.common",
+        "com.back.security"
 })
+@EnableJpaAuditing
 public class ProductApplication {
 
     public static void main(String[] args) {
