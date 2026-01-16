745bc8fcdcefd01b3ff5a74b51808ad3f532e513
jj <syeonie0526@gmail.com>
2026-01-16 10:35:46 +0900

    feat: 입찰 완료 알림 매퍼 추가 - #34

---
+++ notification/src/main/java/com/back/notification/mapper/NotificationMapper.java
@@ -0,0 +1,34 @@
+package com.back.notification.mapper;
+
+import com.back.common.market.event.BiddingCompletedEvent;
+import com.back.notification.domain.Notification;
+import com.back.notification.domain.enums.NotificationTargetRole;
+import com.back.notification.domain.enums.Type;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class NotificationMapper {
+
+    public Notification toBidNotification(Type type, BiddingCompletedEvent event,
+                                          Long userId, NotificationTargetRole role) {
+        return Notification.builder()
+                .userId(userId)
+                .type(type)
+                .content(Map.of(
+                        "biddingId", event.biddingId(),
+                        "productId", event.productId(),
+                        "productName", event.productName(),
+                        "productSize", event.productSize(),
+                        "brandName", event.brandName(),
+                        "thumbnailImage", event.thumbnailImage(),
+                        "price", event.price(),
+                        "biddingPosition", event.biddingPosition(),
+                        "role", role
+                ))
+                .deepLink("/biddings/" + event.biddingId())   // Todo : 실제 딥링크로 수정
+                .isRead(false)
+                .build();
+    }
+}
