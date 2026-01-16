02268314389f3b1cb992f1fa5c8efdfa1b6b5055
Dlrjswn <lkj6387@gmail.com>
2026-01-16 14:43:43 +0900

    chore: 주소값 상수 분리 - #6

--- chat/src/main/java/com/back/chat/mapper/ChatRoomMapper.java
+++ chat/src/main/java/com/back/chat/mapper/ChatRoomMapper.java
@@ -4,7 +4,9 @@ import com.back.chat.domain.ChatRoom;
 import com.back.chat.dto.response.ChatRoomEnterResponseDto;
 
 public class ChatRoomMapper {
+    private static final String CHAT_ROOM_TOPIC_PREFIX = "/topic/chat/room/";
+
     public static ChatRoomEnterResponseDto toEnterResponseDto(ChatRoom chatRoom) {
-        return new ChatRoomEnterResponseDto(chatRoom.getId(), "/topic/chat/room/" + chatRoom.getId());
+        return new ChatRoomEnterResponseDto(chatRoom.getId(), CHAT_ROOM_TOPIC_PREFIX + chatRoom.getId());
     }
 }
