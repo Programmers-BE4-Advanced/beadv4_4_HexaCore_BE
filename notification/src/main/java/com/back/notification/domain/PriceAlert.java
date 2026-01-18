74d4a535f0e7729eb2e8f787265baff198b4398a
jj <syeonie0526@gmail.com>
2026-01-19 00:52:40 +0900

    feat: PriceAlert 엔티티 추가 - #52

---
+++ notification/src/main/java/com/back/notification/domain/PriceAlert.java
@@ -0,0 +1,37 @@
+package com.back.notification.domain;
+
+import com.back.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "price_alerts")
+public class PriceAlert extends BaseTimeEntity {
+    @Id
+    @Column(name = "price_alert_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private NotificationUser user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private NotificationProduct product;
+
+    private BigDecimal targetPrice;
+
+    private LocalDateTime triggeredAt;
+}
