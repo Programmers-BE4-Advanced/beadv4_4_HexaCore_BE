84e05ee65388d539d785593c4e2e72ec80b3a20c
Dlrjswn <lkj6387@gmail.com>
2026-01-19 12:48:36 +0900

    chore: 쿼리 메서드 수정 - #57

--- chat/src/main/java/com/back/chat/adapter/out/ChatMessageRepository.java
+++ chat/src/main/java/com/back/chat/adapter/out/ChatMessageRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
-    Optional<ChatMessage> findTopByChatRoomIdOrderByIdDesc(Long chatRoomId);
+    boolean existsByRoomIdAndUserIdAndContent(
+            Long roomId,
+            Long userId,
+            String content
+    );
 }
