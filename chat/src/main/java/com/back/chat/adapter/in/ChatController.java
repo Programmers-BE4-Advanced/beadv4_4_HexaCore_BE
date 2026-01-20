c3355d6fbd8aa09da5bb194803febb054803c51c
Dlrjswn <lkj6387@gmail.com>
2026-01-20 11:13:18 +0900

    feat: 채팅메시지 히스토리 조회 컨트롤러 메서드 생성 - #58

--- chat/src/main/java/com/back/chat/adapter/in/ChatController.java
+++ chat/src/main/java/com/back/chat/adapter/in/ChatController.java
@@ -1,15 +1,14 @@
 package com.back.chat.adapter.in;
 
 import com.back.chat.app.ChatFacade;
+import com.back.chat.dto.request.ChatMessageHistoryRequestDto;
+import com.back.chat.dto.response.ChatMessageHistoryResponseDto;
 import com.back.chat.dto.response.ChatRoomEnterResponseDto;
 import com.back.chat.mapper.ChatRoomMapper;
 import com.back.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/chat")
@@ -24,6 +23,15 @@ public class ChatController {
                         HttpStatus.OK,
                 "채팅방 입장 성공",
                 chatFacade.enterChatRoom(brandId)
+        );
+    }
+
+    @GetMapping("/history")
+    public CommonResponse<ChatMessageHistoryResponseDto> getHistory(@RequestBody ChatMessageHistoryRequestDto requestDto){
+        return CommonResponse.success(
+                HttpStatus.OK,
+                "채팅 메시지 히스토리 조회 성공",
+                chatFacade.getHistory(requestDto)
         );
     }
 }
