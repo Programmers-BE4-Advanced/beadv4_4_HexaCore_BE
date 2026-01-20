999ea19b0885475b1cb10821beec33a50fc23f5d
NamSangwon <nsw3265@naver.com>
2026-01-20 19:50:59 +0900

    refactor: 상품 생성 메소드 기능 별로 분리하여 메소드 구현 - #29

--- product/src/main/java/com/back/product/app/usecase/ProductUseCase.java
+++ product/src/main/java/com/back/product/app/usecase/ProductUseCase.java
@@ -7,16 +7,13 @@ import com.back.product.domain.*;
 import com.back.product.dto.ProductDto;
 import com.back.product.dto.request.ProductVariantCreateRequestDto;
 import com.back.product.mapper.ProductImageMapper;
-import com.back.product.mapper.ProductInfoMapper;
 import com.back.product.mapper.ProductMapper;
 import com.back.product.mapper.ProductOptionValuesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,33 +37,56 @@ public class ProductUseCase {
         List<ProductImage> createdImages = new ArrayList<>();
 
         variants.forEach(variant -> {
-            // 1. Create Product
-            Product newProduct = productMapper.toEntity(productInfo, variant);
+            Product newProduct = createProduct(productInfo, variant.inventory());
             createdProducts.add(newProduct);
 
-            // 2. Create ProductOptionValues (상품과 옵션 간의 관계 설정)
-            List<ProductOptionValues> newProductOptionValues = variant.optionValueIds().stream().map(optionValueId -> {
-                OptionValue optionValue = optionValueMap.get(optionValueId);
-                return productOptionValuesMapper.toEntity(newProduct, optionValue);
-            }).toList();
+            List<ProductOptionValues> newProductOptionValues = createProductOptionValues(newProduct, variant.optionValueIds(), optionValueMap);
             createdProductOptionValues.addAll(newProductOptionValues);
 
-            // 3. Create ProductImage
-            List<ProductImage> newProductImages = variant.imageUrls().stream()
-                    .map(imageUrl ->  productImageMapper.toEntity(newProduct, imageUrl)).toList();
+            List<ProductImage> newProductImages = createProductImages(newProduct, variant.imageUrls());
             createdImages.addAll(newProductImages);
         });
 
         List<Product> newProducts = productRepository.saveAll(createdProducts);
-        Map<Product, List<ProductOptionValues>> newProductOptionValuesAsMap = productOptionValuesRepository.saveAll(createdProductOptionValues)
-                .stream().collect(Collectors.groupingBy(ProductOptionValues::getProduct));
-        Map<Product, List<ProductImage>> newProductImagesAsMap = productImageRepository.saveAll(createdImages)
-                .stream().collect(Collectors.groupingBy(ProductImage::getProduct));
+        List<ProductOptionValues> newProductOptionValues = productOptionValuesRepository.saveAll(createdProductOptionValues);
+        List<ProductImage> newProductImages = productImageRepository.saveAll(createdImages);
 
-        return newProducts.stream().map(product -> {
-            List<ProductOptionValues> productOptionValues = newProductOptionValuesAsMap.get(product);
-            List<ProductImage> productImages = newProductImagesAsMap.get(product);
-            return productMapper.toDto(product, productOptionValues, productImages);
+        return convertToDto(newProducts, newProductOptionValues, newProductImages);
+    }
+    
+    private Product createProduct(ProductInfo productInfo, Long inventory) {
+        return productMapper.toEntity(productInfo, inventory);
+    }
+
+    private List<ProductOptionValues> createProductOptionValues(Product product, List<Long> optionValueIds, Map<Long, OptionValue> optionValueMap) {
+        return optionValueIds.stream().map(optionValueId -> {
+            OptionValue optionValue = optionValueMap.get(optionValueId);
+            return productOptionValuesMapper.toEntity(product, optionValue);
         }).toList();
+    }
+
+    private List<ProductImage> createProductImages(Product product, List<String> imageUrls) {
+        return imageUrls.stream().map(imageUrl ->
+                productImageMapper.toEntity(product, imageUrl)
+        ).toList();
+    }
+
+    private List<ProductDto> convertToDto(
+            List<Product> products,
+            List<ProductOptionValues> productOptionValues,
+            List<ProductImage> productImages
+    ) {
+        Map<Product, List<ProductOptionValues>> productOptionValuesAsMap = productOptionValues
+                .stream().collect(Collectors.groupingBy(ProductOptionValues::getProduct));
+        Map<Product, List<ProductImage>> productImagesAsMap = productImages.stream()
+                .collect(Collectors.groupingBy(ProductImage::getProduct));
+
+        return products.stream().map(product ->
+            productMapper.toDto(
+                product,
+                productOptionValuesAsMap.get(product),
+                productImagesAsMap.get(product)
+            )
+        ).toList();
     }
 }
