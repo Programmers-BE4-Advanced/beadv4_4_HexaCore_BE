28c9cb185628967b6245c1974b95d1db146616fb
NamSangwon <nsw3265@naver.com>
2026-01-21 12:11:19 +0900

    fix: Query 어노테이션의 변수 명시를 위한 Param 어노테이션 추가 - #29

--- product/src/main/java/com/back/product/adapter/out/ProductImageRepository.java
+++ product/src/main/java/com/back/product/adapter/out/ProductImageRepository.java
@@ -5,6 +5,7 @@ import com.back.product.domain.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
     @Modifying
     @Query("update ProductImage pi set pi.deletedAt = now() where pi.product in :products")
-    void deleteAllByProductIn(List<Product> existsProducts);
+    void deleteAllByProductIn(@Param("products") List<Product> existsProducts);
 
     List<ProductImage> findALlByProductIn(List<Product> products);
 }
--- product/src/main/java/com/back/product/adapter/out/ProductOptionValuesRepository.java
+++ product/src/main/java/com/back/product/adapter/out/ProductOptionValuesRepository.java
@@ -5,13 +5,14 @@ import com.back.product.domain.ProductOptionValues;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ProductOptionValuesRepository extends JpaRepository<ProductOptionValues, Long> {
     @Modifying
     @Query("update ProductOptionValues pov set pov.deletedAt = now() where pov.product in :products")
-    void deleteAllByProductIn(List<Product> products);
+    void deleteAllByProductIn(@Param("products") List<Product> products);
 
     List<ProductOptionValues> findAllByProductIn(List<Product> products);
 }
