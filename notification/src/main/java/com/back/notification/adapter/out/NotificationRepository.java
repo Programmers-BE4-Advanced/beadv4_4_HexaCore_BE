cf6aec9d1f927b8ca2d8efec35fc286808ff94df
jj <syeonie0526@gmail.com>
2026-01-21 10:08:40 +0900

    feat: Mongo 알림 읽음 처리 Custom Repository 구현- #73

--- notification/src/main/java/com/back/notification/adapter/out/NotificationRepository.java
+++ notification/src/main/java/com/back/notification/adapter/out/NotificationRepository.java
@@ -3,5 +3,6 @@ package com.back.notification.adapter.out;
 import com.back.notification.domain.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NotificationRepository extends MongoRepository<Notification, String> {
+public interface NotificationRepository extends MongoRepository<Notification, String>,
+        NotificationRepositoryCustom {
 }
---
+++ notification/src/main/java/com/back/notification/adapter/out/NotificationRepositoryCustom.java
@@ -0,0 +1,5 @@
+package com.back.notification.adapter.out;
+
+public interface NotificationRepositoryCustom {
+    void markAsRead(String notificationId);
+}
