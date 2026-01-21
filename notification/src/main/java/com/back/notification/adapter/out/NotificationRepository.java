21876174b67631a5bac08eb3456cb2be1a0c5974
jj <syeonie0526@gmail.com>
2026-01-21 11:10:14 +0900

    feat: 알림 조회용 Repository 및 Support 구현 - #73

--- notification/src/main/java/com/back/notification/adapter/out/NotificationRepository.java
+++ notification/src/main/java/com/back/notification/adapter/out/NotificationRepository.java
@@ -1,8 +1,11 @@
 package com.back.notification.adapter.out;
 
 import com.back.notification.domain.Notification;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface NotificationRepository extends MongoRepository<Notification, String>,
         NotificationRepositoryCustom {
+    Slice<Notification> findPageByUserId(Pageable pageable, Long userId);
 }
