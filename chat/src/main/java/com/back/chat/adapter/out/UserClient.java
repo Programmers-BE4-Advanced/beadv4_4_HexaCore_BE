10a4f0f9c48a1fe65fab71eb5f15020be8cf8bf2
Dlrjswn <lkj6387@gmail.com>
2026-01-21 16:36:55 +0900

    feat: User 모듈 ChatRestrictedUntil 조회 요청 구현 - #85

--- chat/src/main/java/com/back/chat/adapter/out/UserClient.java
+++ chat/src/main/java/com/back/chat/adapter/out/UserClient.java
@@ -1,9 +1,12 @@
 package com.back.chat.adapter.out;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
 
 @FeignClient(name = "userClient", url = "${user-service.url}")
 public interface UserClient {
@@ -13,4 +16,7 @@ public interface UserClient {
             @PathVariable("userId") Long userId,
             @RequestParam("messageId") Long messageId
     );
+
+    @GetMapping("/internal/users/{userId}/chat-restricted")
+    LocalDateTime getChatRestrictedUntil(@PathVariable("userId") Long userId);
 }
